bd985b300bb0c4b47e2694375c9caa422180f1cf
IEHAG <hectorabadgomezz@gmail.com>
2025-02-22 07:59:53 -0500

    2025

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nombre-del-repositorio/', // ⚠️ Reemplaza con el nombre exacto de tu repositorio en GitHub
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
